4c79dd6c7a343ed3e2981d437948d887e1b4e5ed
Nart <daniel.abzakh@gmail.com>
2020-09-01 12:13:00 +0300

    Modified run server file

--- server/run.py
+++ server/run.py
@@ -13,16 +13,12 @@ def index():
 def translate():
     tgt_list = None
     language = request.form['langSrc'] + '-' + request.form['langTgt']
-    sp_path_src = app.root_path +"/util/src.model"
-    sp_path_tgt = app.root_path +"/util/tgt.model"
-    ct_path_ab = app.root_path + "/ctranslate_model_ab"
-    ct_path_ru = app.root_path + "/ctranslate_model_ru"
+    sp_path_src = app.root_path +"/models/"+language+"/src.model"
+    sp_path_tgt = app.root_path +"/models/"+language+"/tgt.model"
+    model_path = app.root_path +"/models/"+language
     if 'source' in request.form:
         src_list = request.form['source'].split("\n")
-        if language == "ab-ru":
-            tgt_list = process.translate(src_list,sp_path_src,sp_path_tgt,ct_path_ab)
-        elif language == "ru-ab":
-            tgt_list = process.translate(src_list,sp_path_tgt,sp_path_src,ct_path_ru)
+        tgt_list = process.translate(src_list,sp_path_src,sp_path_tgt,model_path)
         return jsonify({'target':"\n".join(tgt_list)})
     elif 'file' in request.files:
         file = request.files['file']
