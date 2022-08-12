a70a9530eb33cd8237e956286d9d441847d99fcb
Nart <daniel.abzakh@gmail.com>
2022-08-12 13:14:51 +0300

    edited setup script

--- server/setup.sh
+++ server/setup.sh
@@ -1,4 +1,5 @@
 sudo apt-get install curl
+sudo apt-get install python3-pip
 echo "deb http://storage.googleapis.com/tensorflow-serving-apt stable tensorflow-model-server tensorflow-model-server-universal" | sudo tee /etc/apt/sources.list.d/tensorflow-serving.list && \
 curl https://storage.googleapis.com/tensorflow-serving-apt/tensorflow-serving.release.pub.gpg | sudo apt-key add -
 sudo apt update
