ffd6461f606aab7b138c59dfc08625e91de21bd2
Nart <daniel.abzakh@gmail.com>
2022-10-10 17:35:46 +0300

    left margin fix

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
-
+  darkMode: string;
   constructor() { }
 
   ngOnInit(): void {
+    if (!localStorage.getItem("darkMode"))
+      localStorage.setItem("darkMode", "light");
+    this.darkMode = localStorage.getItem("darkMode");
+  }
+
+  toggleDarkMode() {
+    let body = document.getElementsByTagName("body");
+    if (this.darkMode == "light") {
+      localStorage.setItem("darkMode", "dark");
+      this.darkMode = "dark";
+      console.log(body)
+    
+
+    }
+    else {
+      localStorage.setItem("darkMode", "light");
+      this.darkMode = "light";
+
+    }
+
   }
 
 }
