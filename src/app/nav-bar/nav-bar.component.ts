f998822b4d76a1f819396114ffd84da35d647ce3
Nart <daniel.abzakh@gmail.com>
2022-10-11 15:40:56 +0300

    Added dark mode

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -6,30 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
-  darkMode: string;
   constructor() { }
 
   ngOnInit(): void {
     if (!localStorage.getItem("darkMode"))
-      localStorage.setItem("darkMode", "light");
-    this.darkMode = localStorage.getItem("darkMode");
+      localStorage.setItem("darkMode", "");
+    document.body.classList.add(localStorage.getItem("darkMode"));
   }
 
   toggleDarkMode() {
-    let body = document.getElementsByTagName("body");
-    if (this.darkMode == "light") {
+    let isAdded = document.body.classList.toggle("dark");
+    if (isAdded) {
       localStorage.setItem("darkMode", "dark");
-      this.darkMode = "dark";
-      console.log(body)
-    
-
     }
     else {
-      localStorage.setItem("darkMode", "light");
-      this.darkMode = "light";
-
+      localStorage.setItem("darkMode", "");
     }
-
   }
 
 }
