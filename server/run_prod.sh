56fd797e61eb04b73833c843b2bfba8b4e288680
Nart <daniel.abzakh@gmail.com>
2023-03-28 13:46:28 +0300

    fix

--- server/run_prod.sh
+++ server/run_prod.sh
@@ -1,4 +1,4 @@
-gunicorn --certfile /etc/letsencrypt/live/server.bagrat.space/fullchain.pem --keyfile /etc/letsencrypt/live/server.bagrat.space/privkey.pem -b server.bagrat.space:5000 run:app >flask.log 2>&1 &
+sudo gunicorn --certfile /etc/letsencrypt/live/server.bagrat.space/fullchain.pem --keyfile /etc/letsencrypt/live/server.bagrat.space/privkey.pem -b server.bagrat.space:5000 run:app >flask.log 2>&1 &
 python3 models-config.py
 nohup tensorflow_model_server \
   --rest_api_port=8501 \
